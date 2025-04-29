07443df0e84acc1a0e29323dc1b9d4c993137a2c
coding-engineer-1701 <alexander.g.bush@gmail.com>
2025-04-29 19:39:38 +1000

    Created header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// TODO - create a header that changes after login
+
+function Header() {
+    return (
+        <header>
+            <div>
+                <p class="welcome-message">jammming</p>
+            </div>
+        </header>
+    )
+}
+
+
+export default Header;
+
